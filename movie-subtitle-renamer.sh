1ca7afa84637e7a71d3b7f78990ac1cf6cd9e1ba
Patrik Piskay <patrik.piskay@pianomedia.com>
2015-08-11 15:11:15 +0200

    separator option added, refactoring

--- movie-subtitle-renamer.sh
+++ movie-subtitle-renamer.sh
@@ -2,41 +2,52 @@
 
 shopt -s nocasematch
 
+separator=$1
+if [[ -z $separator ]]; then
+    # default separator
+    separator="-"
+fi
+
 function renameFiles {
     for item in *
     do
         # if it is file...
-        if [[ -f "$item" ]];
-        then
+        if [[ -f "$item" ]]; then
             local fileName=$item
 
             # find subtitles
-            # if [[ $fileName =~ ^(.*?)(s[0-9]{1,2}e[0-9]{1,2})+(\([0-9]+\))?\.(srt|sub)$ ]]; 
-            if [[ $fileName =~ ^(.*?)(\([0-9]+\))?\.(srt|sub)$ ]];
-            then
-                local namePart1=${BASH_REMATCH[1]}
-                local namePart2=${BASH_REMATCH[2]}
-                local name=$namePart1$namePart2
+            if [[ $fileName =~ ^([^\(\)]*)(\([0-9]+\))?\.(sub|srt)$ ]]; then
+                local subName=${BASH_REMATCH[1]}
+                local subCleanName=$subName
+
+                # subName contains Season/Episode part (TV show)
+                if [[ $subName =~ ^(.*)(s[0-9]{1,2}e[0-9]{1,2}) ]]; then
+                    local showName=${BASH_REMATCH[1]}
+                    local showSeasonEpisode=${BASH_REMATCH[2]}
+                    subCleanName=$showName$showSeasonEpisode
+                # subName contains some movie format information
+                # hdtv|19[0-9]{2}|20[0-9]{2}|hd-?ts|xvid|hdrip|brrip|x264|480p|720p|1080p|ac3-ev
+                # elif [[ $subName =~ ... ]]; then
+                    # subCleanName=${BASH_REMATCH[1]}
+                fi
 
                 # replace "- _" characters with "." so when used as regex it will match files 
                 # that contain the same name disregarding the separator used
-                local nameRegex="${name//[-\ _]/\.}"
+                local nameRegex="${subCleanName//[-\ _]/\.}"
+
                 # replace " _." separators for dash separator
-                local nameClean="${name//[\ _\.]/-}"
+                local nameFormatted="${subCleanName//[-\ _\.]/$separator}"
 
                 for fileName2 in *
                 do
-                    if [[ -f "$fileName2" ]];
-                    then
+                    if [[ -f "$fileName2" ]]; then
                         # this should pass for both movie and subtitle file because of the name regex 
                         # (provided both movie and subtitle file use the same movie name as its base)
-                        if [[ $fileName2 =~ ^$nameRegex(.*?)\.([a-z0-9]{3,4})$ ]];
-                        then
+                        if [[ $fileName2 =~ ^$nameRegex(.*?)\.([a-z0-9]{3,4})$ ]]; then
                             local extension=${BASH_REMATCH[2]}
-                            local newName=$nameClean.$extension
+                            local newName=$nameFormatted.$extension
 
-                            if [ ! -e "$newName" ];
-                            then
+                            if [ ! -e "$newName" ]; then
                                 # mv "$fileName2" "$newName"
                                 echo File renamed from "$fileName2" to "$newName"
                             fi
@@ -45,16 +56,16 @@ function renameFiles {
                 done
             fi
         # if it is directory...
-        elif [[ -d "$item" ]];
-        then
+        elif [[ -d "$item" ]]; then
             cd "$item"
-            renameFiles
+            renameFiles $separator
             cd ..
         fi
     done
 }
 
-renameFiles
+# run
+renameFiles $separator
 
 shopt -u nocasematch
 
